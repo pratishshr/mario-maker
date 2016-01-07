218a39b23935830402cf5443d756a621308b769f
pratishshr <vanroshr@gmail.com>
2016-01-07 15:49:09 +0545

    bug fixed

--- js/facebook.js
+++
@@ -1,7 +0,0 @@
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=716944565071987";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
