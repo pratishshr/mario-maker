6a1d3a25f84db6504823555ecfa28d176a437f39
pratishshr <vanroshr@gmail.com>
2016-01-07 15:51:06 +0545

    instructions fixed

--- js/mainGame/MarioGame.js
+++ js/mainGame/MarioGame.js
@@ -212,7 +212,7 @@ function MarioGame() {
 
   this.showInstructions = function() {
     gameUI.writeText('Controls: Arrow keys for direction, shift to run, ctrl for bullets', 30, 30)
-    gameUI.writeText('Tip: Jumping while running makes to jump higher', 30 , 60);
+    gameUI.writeText('Tip: Jumping while running makes you jump higher', 30 , 60);
   }
 
   this.renderMap = function() {
